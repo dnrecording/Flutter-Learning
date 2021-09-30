682e54229eadc10ac1c308068a6427c8fb64bdea
drgn <62900543+dnrecording@users.noreply.github.com>
2021-10-01 00:00:21 +0700

    2:22:00

--- learning_flutter/lib/main.dart
+++ learning_flutter/lib/main.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  var app = MaterialApp(
-    title: "myApp",
-    home: Scaffold(
-      appBar: AppBar(title: Text("Hello from AppBar")),
-      body: Text("Hello from body"),
-    ),
-    theme: ThemeData(primarySwatch: Colors.red),
-  );
-  runApp(app);
+  runApp(MyApp());
+}
+
+// widget สร้างเองแบบ Stateless
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "myApp",
+      home: Scaffold(
+        appBar: AppBar(title: Text("Hello from AppBar")),
+        body: Center(child: Text("Hello from body")),
+      ),
+      theme: ThemeData(primarySwatch: Colors.red),
+    );
+  }
 }
