94a6d3781b30568503539a9224bbbcc75dada966
drgn <62900543+dnrecording@users.noreply.github.com>
2021-10-02 00:07:47 +0700

    Stateful wid + setState

--- learning_flutter/lib/main.dart
+++ learning_flutter/lib/main.dart
@@ -11,12 +11,47 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "myApp",
-      home: Scaffold(
-        appBar: AppBar(title: Text("Hello from AppBar")),
-        body: Center(child: Text("Hello from body")),
+      title: "App",
+      home: MyHomePage(),
+      theme: ThemeData(primarySwatch: Colors.indigo),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+int number = 0; //set state
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text(
+        "Number Counter",
+        style: TextStyle(color: Colors.amber[300]),
+      )),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("Counting", style: TextStyle(color: Colors.amber[900])),
+          Text("$number", style: TextStyle(fontSize: 36))
+        ],
+      )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            number++;
+          });
+        },
+        child: Text("press"),
       ),
-      theme: ThemeData(primarySwatch: Colors.red),
     );
   }
 }
